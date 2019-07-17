fb85cdd6b004a6e50cb3e48ed4c032d5c38bb0a2
Omelchenko Michael <l2maximum@mail.ru>
2019-07-17 08:38:10 +0300

    update contact verbose_name in admin

--- oms_cms/backend/contact/apps.py
+++ oms_cms/backend/contact/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class ContactConfig(AppConfig):
-    name = 'contact'
+    name = 'oms_cms.backend.contact'
+    verbose_name = _('Контакты')
