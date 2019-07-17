0f0feaf894f4d52f12cee8918c165ee12d568e26
Omelchenko Michael <l2maximum@mail.ru>
2019-07-17 08:53:04 +0300

    update utils verbose_name in admin

--- oms_cms/backend/utils/apps.py
+++ oms_cms/backend/utils/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class UtilsConfig(AppConfig):
-    name = 'utils'
+    name = 'oms_cms.backend.utils'
+    verbose_name = _('Настройки')
