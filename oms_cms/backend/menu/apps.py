d59841a2645eaa3d3f63b86a113e42fa667b2f93
Omelchenko Michael <l2maximum@mail.ru>
2019-07-17 08:43:32 +0300

    update menu verbose_name in admin

--- oms_cms/backend/menu/apps.py
+++ oms_cms/backend/menu/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class MenuConfig(AppConfig):
-    name = 'menu'
+    name = 'oms_cms.backend.menu'
+    verbose_name = _('Меню')
