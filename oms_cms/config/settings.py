1b1db9307c8255689bef46f3352c9ccc7cb254ea
Omelchenko Michael <l2maximum@mail.ru>
2019-07-17 16:23:40 +0300

    update contact

--- oms_cms/config/settings.py
+++ oms_cms/config/settings.py
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    #'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
