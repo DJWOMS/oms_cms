e55feceda4ba347858fbf5486dba37edafc31f57
Omelchenko Michael <l2maximum@mail.ru>
2019-07-17 08:47:47 +0300

    update search verbose_name in admin

--- oms_cms/backend/search/apps.py
+++ oms_cms/backend/search/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class SearchConfig(AppConfig):
-    name = 'search'
+    name = 'oms_cms.backend.search'
+    verbose_name = _('Поиск')
