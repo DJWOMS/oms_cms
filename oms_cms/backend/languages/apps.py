08d5bbca841b3ffd841e0b6c6369250c759ea669
Omelchenko Michael <l2maximum@mail.ru>
2019-07-17 08:43:16 +0300

    update languages verbose_name in admin

--- oms_cms/backend/languages/apps.py
+++ oms_cms/backend/languages/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class LanguagesConfig(AppConfig):
-    name = 'languages'
+    name = 'oms_cms.backend.languages'
+    verbose_name = _('Языки')
