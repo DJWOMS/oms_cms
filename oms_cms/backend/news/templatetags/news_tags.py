9c7c2690887fd2d08c9ca13a367c4e7247bead7a
Omelchenko Michael <l2maximum@mail.ru>
2019-07-26 15:55:10 +0300

    add template tags news non html

--- oms_cms/backend/news/templatetags/news_tags.py
+++ oms_cms/backend/news/templatetags/news_tags.py
@@ -12,13 +12,29 @@ def category_list(template='base/tags/news/categories.html'):
     return {'template': template, "category_list": categories}
 
 
+@register.simple_tag()
+def for_category_list():
+    """template tag вывода категорий без шаблона"""
+    categories = Category.objects.filter(published=True)
+    return categories
+
+
 @register.inclusion_tag('base/tags/base_tag.html')
 def post_list(category=None, template='base/tags/news/news_block_tag.html'):
-    """Вывод списка статей из категории или всех"""
+    """Вывод списка статей из категории или всех в шаблон"""
     if category is not None:
         context = Post.objects.filter(category__name__icontains=category)
     else:
         context = Post.objects.all()
     return {'template': template, "post_list": context}
 
+
+@register.simple_tag()
+def for_post_list(category=None):
+    """Вывод списка статей из категории или всех"""
+    if category is not None:
+        context = Post.objects.filter(category__name__icontains=category)
+    else:
+        context = Post.objects.all()
+    return context
 
