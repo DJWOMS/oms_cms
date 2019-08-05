c262597bbd6cfa2c9843aea886a98eca239efe7f
Omelchenko Michael <l2maximum@mail.ru>
2019-08-05 16:29:40 +0300

    update admin & models languages

--- oms_cms/backend/languages/admin.py
+++ oms_cms/backend/languages/admin.py
@@ -6,5 +6,6 @@ from .models import Lang
 @admin.register(Lang)
 class LangAdmin(admin.ModelAdmin):
     """Языки"""
-    list_display = ("name", "slug")
+    list_display = ("name", "slug", "is_default")
+    list_editable = ("is_default", )
     # prepopulated_fields = {"slug": ("name",)}
