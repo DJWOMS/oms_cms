8e07485277fd3a80b4bf983d6f9a18425c98c8cf
Omelchenko Michael <l2maximum@mail.ru>
2019-08-14 16:14:01 +0300

    add filter for tags in news

--- oms_cms/backend/news/templatetags/news_tags.py
+++ oms_cms/backend/news/templatetags/news_tags.py
@@ -6,35 +6,44 @@ register = template.Library()
 
 
 @register.inclusion_tag('base/tags/base_tag.html')
-def category_list(template='base/tags/news/categories.html'):
+def category_list(order="-name", count=None, template='base/tags/news/categories.html'):
     """template tag вывода категорий"""
-    categories = Category.objects.filter(published=True)
+    categories = Category.objects.filter(published=True).order_by(order)
+    if count is not None:
+        categories = categories[:count]
     return {'template': template, "category_list": categories}
 
 
 @register.simple_tag()
-def for_category_list():
+def for_category_list(order="-name", count=None):
     """template tag вывода категорий без шаблона"""
-    return Category.objects.filter(published=True)
+    categories = Category.objects.filter(published=True).order_by(order)
+    if count is not None:
+        categories = categories[:count]
+    return categories
 
 
 @register.inclusion_tag('base/tags/base_tag.html')
-def post_list(category=None, template='base/tags/news/news_block_tag.html'):
+def post_list(category=None, order="-published_date", count=None, template='base/tags/news/news_block_tag.html'):
     """Вывод списка статей из категории или всех в шаблон"""
     if category is not None:
-        context = Post.objects.filter(category__name__icontains=category)
+        context = Post.objects.filter(category__name__icontains=category).order_by(order)
     else:
-        context = Post.objects.all()
+        context = Post.objects.order_by(order)
+    if count is not None:
+        context = context[:count]
     return {'template': template, "post_list": context}
 
 
 @register.simple_tag()
-def for_post_list(category=None):
+def for_post_list(category=None, order="-published_date", count=None):
     """Вывод списка статей из категории или всех"""
     if category is not None:
-        context = Post.objects.filter(category__name__icontains=category)
+        context = Post.objects.filter(category__name__icontains=category).order_by(order)
     else:
-        context = Post.objects.all()
+        context = Post.objects.order_by(order)
+    if count is not None:
+        context = context[:count]
     return context
 
 
--- oms_cms/backend/news/urls.py
+++ oms_cms/backend/news/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("comment-edit/<int:pk>/", EditComment.as_view(), name="edit_comment"),
     path("comment-answer/<int:pk>/", AnswerComment.as_view(), name="answer_comment"),
     path("comment-del/<int:pk>/", DeleteComment.as_view(), name="delete_comment"),
+    path("tag/<slug:tag>/", PostView.as_view(), name="tag-news"),
     path("<slug:category>/<slug:post>/", PostDetail.as_view(), name="new-detail"),
     path("<slug:slug>/", PostView.as_view(), name="list-news"),
     path("", PostView.as_view(), name="all-news"),
