9b1768286811bc3b01b8c88fcee6c8f31a1d03e7
Omelchenko Michael <l2maximum@mail.ru>
2019-08-29 15:30:25 +0300

    add published menu

--- oms_cms/backend/menu/admin.py
+++ oms_cms/backend/menu/admin.py
@@ -1,23 +1,28 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
+
+from oms_cms.backend.utils.admin import ActionPublish
 from .models import Menu, MenuItem
 from .forms import MenuItemAdminForm
 
 
 @admin.register(Menu)
-class MenuAdmin(admin.ModelAdmin):
+class MenuAdmin(ActionPublish):
     """Меню"""
-    list_display = ("name", "status")
+    list_display = ("name", "status", "published")
+    list_filter = ("published",)
+    actions = ['unpublish', 'publish']
 
 
 @admin.register(MenuItem)
-class MenuItemAdmin(MPTTModelAdmin):
+class MenuItemAdmin(MPTTModelAdmin, ActionPublish):
     """Пункты меню"""
     form = MenuItemAdminForm
-    list_display = ("title", "name", "parent", "lang", "menu", "sort", "id")
-    list_filter = ("menu", "parent", "lang")
+    list_display = ("title", "name", "parent", "lang", "menu", "sort", "id", "published")
+    list_filter = ("menu", "parent", "lang", "published")
     search_fields = ("name", "parent", "menu")
     save_as = True
     list_editable = ("sort", )
     mptt_level_indent = 20
+    actions = ['unpublish', 'publish']
 
