89be9938a72d2a0707e0f47a037561ce3ae106a0
Omelchenko Michael <l2maximum@mail.ru>
2019-09-16 16:47:18 +0300

    update command addpage & addcontact

--- oms_cms/backend/pages/management/commands/addpage.py
+++ oms_cms/backend/pages/management/commands/addpage.py
@@ -8,12 +8,9 @@ class Command(BaseCommand):
     help = 'Add page'
 
     def handle(self, *args, **options):
-        text_home = """<h3>Language</h3>
-                 <p>OMS is written in Python, the fastest growing programming language.</p>
-                 <h3>Sites</h3>
-                 <p>OMS sites are immediately operational after installation and easily expandable.</p>
-                 <h3>Security</h3>
-                 <p>OMS is developed on Django which includes security.</p>"""
+        text_home = """The system is open source, written using the Django framework in the Python 
+            programming language. This cms allows you to make a website in minutes. You can use the 
+            basic template or download from the official site."""
         text_about = """<h1>Install OMS CMS in just 4 steps</h1>
                     <p>pip install oms-cms</p>
                     <p>oms-start</p>
@@ -22,7 +19,7 @@ class Command(BaseCommand):
         Pages.objects.create(
             title="Home",
             text=text_home,
-            slug=None,
+            slug="/",
             lang=settings.LANGUAGE_CODE
         )
         Pages.objects.create(
